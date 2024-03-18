0a650803c42da5e4d6f8de64962711eb1a852634
Patrick <patrick.h.th@gmail.com>
2024-03-18 17:01:22 +0100

    Exercise 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
+#include <QCoreApplication>
+#include <QtSql>
+#include <QSqlDatabase>
 
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+void exercise1()
+{
+    QSqlQuery query;
+
+    qDebug() << query.exec("select e.fname, e.address from department as d, employee as e where dname like \"research\";");
+
+    qDebug() << "fname | " << "adress";
+    while (query.next())
+    {
+        QString fname = query.value(0).toString();
+        QString adress = query.value(1).toString();
+
+         qDebug() << fname << " | "  << adress;
+    }
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("company");
+    db.setUserName("root");  // Change to username
+    db.setPassword("Tqk97wew.");  // Change to password
+    db.open();
+
+
+    // Exercise 1
+    exercise1();
+
     return 0;
 }
